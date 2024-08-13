323586299df5cf455ce9f4e5f1aebc9351e3346b
蒋固金 <1013195469@qq.com>
2024-08-13 14:27:37 +0800

    构建调整

--- internal/ollama/cmd/check_darwin.go
+++ internal/ollama/cmd/check_darwin.go
@@ -13,11 +13,11 @@ import (
 func CheckInstalled(ctx context.Context) (bool, error) {
 	exe, err := os.Executable()
 	if err != nil {
-		return err
+		return false, err
 	}
 	link, err := os.Readlink(exe)
 	if err != nil {
-		return err
+		return false, err
 	}
 	if !strings.Contains(link, "Ollama.app") {
 		return false, fmt.Errorf("could not find ollama app")
